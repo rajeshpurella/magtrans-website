c454e19036ccbab4a7b0eba105f864e3f3661a8e
Rajesh Purella <mee-github-email@gmail.com>
2026-03-07 18:23:58 +0530

    back button changes

--- app/industries/layout.tsx
+++ app/industries/layout.tsx
@@ -4,7 +4,7 @@ import BackButton from "../components/BackButton";
 export default function IndustriesLayout({ children }: { children: ReactNode }) {
   return (
     <>
-      <div className="pt-28 pb-6 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto">
+      <div className="mt-6 pb-6 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto">
         <BackButton />
       </div>
       {children}
